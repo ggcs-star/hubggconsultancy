d3c7e2183810fd65c2932c4069a0d8e1d49872f0
ggcs-star <ggcs@globalgarner.com>
2026-09-07 19:45:56 +0530

    changes done

--- resources/views/components/checklist-dots.blade.php
+++ resources/views/components/checklist-dots.blade.php
@@ -7,11 +7,7 @@
                 title="{{ $item->title }}"
                 class="flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-[10px] font-bold {{ $onPlatform && $item->completed ? 'bg-emerald-500 text-white' : 'bg-slate-100 text-slate-400' }}"
             >
-                @if ($onPlatform && $item->completed)
-                    <x-icon name="check" class="h-3 w-3" />
-                @else
-                    {{ mb_strtoupper(mb_substr($item->title, 0, 1)) }}
-                @endif
+                {{ mb_strtoupper(mb_substr($item->title, 0, 1)) }}
             </span>
         @endforeach
     </div>
